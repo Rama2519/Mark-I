2f4c1c20fcc219d2f6628498897c938651741ca9
Rama Sathya Sai <ramasathyasai2519@gmail.com>
2022-05-28 18:58:15 +0530

    Repository created

---
+++ main.py
@@ -0,0 +1,49 @@
+import wikipedia
+from variables import *
+
+
+# Open Website
+if command[0] == "open" and command[2] == "website":
+    command.remove('open')
+    command.remove('website')
+    command = list_to_string(command)
+    website(command)
+# Open App
+elif command[0] == "open":
+    command.remove('open')
+    command = list_to_string(command)
+    speak_text(opening + command)
+    open_app(command)
+# Play my music
+elif command == ['play', 'my', 'music']:
+    my_music()
+# Play music
+elif command[0] == "play":
+    command.remove("play")
+    command = list_to_string(command)
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        speak_text(options)
+        print('listening...')
+        audio = r.listen(source)
+        user_input = [r.recognize_google(audio)]
+    user_input = convert(user_input)
+    speak_text(playing + command)
+    if user_input[0] == "Amazon":
+        music_amazon(command)
+    else:
+        music_yt(command)
+# Wikipedia search
+elif command[0] == "what" or command[0] == "who":
+    command = list_to_string(command)
+    print(wikipedia.summary(command, sentences=2))
+    speak_text(wikipedia.summary(command, sentences=2))
+# Pip install
+elif command[0] == "PIP":
+    print('installing...')
+    command = list_to_string(command)
+    command.lower()
+    open_cmd(command)
+else:
+    print('none')
---
+++ variables.py
@@ -0,0 +1,109 @@
+import pyttsx3
+import pyautogui as pag
+from selenium import webdriver
+from time import sleep as s
+from selenium.webdriver.common.by import By
+import speech_recognition as sr
+
+opening = "opening"
+playing = "playing"
+alexa = "alexa"
+listening = "How can I help you?: "
+options = "Do you want me to play on YouTube or Amazon Music?"
+
+
+def speak_text(x):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[2].id)
+    engine.say(x)
+    engine.runAndWait()
+
+
+def convert(x):
+    return x[0].split()
+
+
+def list_to_string(x):
+    str1 = " "
+    return str1.join(x)
+
+
+def music_amazon(x):
+    pag.press("win")
+    pag.write("amazon")
+    pag.press("enter")
+    s(0.5)
+    pag.hotkey('win', 'up')
+    s(3)
+    pag.write(x)
+    pag.press('enter')
+    s(1)
+    pag.moveTo(121, 475)
+    pag.click()
+
+
+def music_yt(x):
+    yt = webdriver.Chrome("C:/Users/ramas/chromedriver.exe")
+    yt.maximize_window()
+    yt.get('https://www.youtube.com')
+    s(0.5)
+    pag.press("/")
+    s(0.5)
+    pag.write(x)
+    pag.press("enter")
+    s(1)
+    yt.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div['
+                              '1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div['
+                              '2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]').click()
+    while True:
+        pass
+
+
+def website(x):
+    ws = webdriver.Chrome("C:/Users/ramas/chromedriver.exe")
+    ws.maximize_window()
+    ws.get('https://www.' + x + '.com')
+    while True:
+        pass
+
+
+def open_app(x):
+    pag.press("win")
+    pag.write(x)
+    pag.press("enter")
+
+
+def my_music():
+    open_app("amazon")
+    s(3)
+    pag.hotkey('win', 'up')
+    pag.moveTo(531, 72)
+    pag.click()
+    pag.moveTo(507, 173)
+    pag.click()
+    pag.moveTo(308, 150)
+    pag.click()
+    pag.moveTo(168, 454)
+    pag.click()
+
+
+def open_cmd(x):
+    pag.press('win')
+    pag.write('cmd')
+    pag.press('enter')
+    s(0.5)
+    pag.write(x)
+    pag.press('enter')
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source, duration=0.2)
+    speak_text("How can I help you?")
+    print('listening.....')
+    audio = r.listen(source)
+    user_input = [r.recognize_google(audio)]
+
+command = convert(user_input)
